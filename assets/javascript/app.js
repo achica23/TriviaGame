c283213b940e0e85c702473da8391964ee245772
Ashley Chica <ahaven11@gmail.com>
2020-05-02 23:54:14 -0400

    files edited, added jumbrotron and questions, created array

---
+++ assets/javascript/app.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+    // create variable to track questions answers
+    var trackQuestion = 0;
+
+    // create variable for correct and incorrect answers
+    var correctAnswer = 0;
+    var incorrectAnswer = 0;
+
+    // create array of questions, answer options, and correct answer
+    var questions = [
+        {
+            question: "Season 1 Episode 1: Who is King of the North?",
+            choices: ["Tyrion Lannister", "Theon Greyjoy", "Ramsey Bolton", "Gendry", "Ned Stark"],
+            choicesAnswer: 4
+        },
+        {
+            question: "What is the name of Arya's direwolf?",
+            choices: ["Summer", "Nymeria", "Grey Wind", "Lady", "Ghost"],
+            choicesAnswer: 1
+        },
+        {
+            question: "Valar Morghulis",
+            choices: ["Valar Doharis", "Valar Rohnas", "Valar GoGo"],
+            choicesAnswer: 0
+        }];
+
+    // function to submit answers
+    function submitAnswer() { }
+
+    // function to display countdown timer - top of page 
+    function displayTime() { }
+
+    // function to 
+
+
+
+
+
+
+
+
+
+
+})
